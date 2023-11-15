55f8faf90213145cfb085ce4044cce6d34e76753
Michael Adamson <michaelnjadamson@outlook.com>
2023-11-14 21:40:19 -0500

    in working order

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -14,15 +14,16 @@ export default class App extends React.Component {
     const { value } = evt.target
     this.setState({...this.state, todoNameInput: value})
   }
+
+  resetForm = () => this.setState({...this.state, todoNameInput: ''})
+  setAxiosResponseError = err => this.setState({...this.state, error: err.response.data.message })
   postNewTodo = () => {
     axios.post(URL, { name: this.state.todoNameInput })
       .then(res => {
-        this.fetchAllTodos()
-        this.setState({...this.state, todoNameInput: ''})
+        this.setState({...this.state, todos: this.state.todos.concat(res.data.data)})
+        this.resetForm()
       })
-      .catch(err => {
-        this.setState({...this.state, error: err.response.data.message })
-      })
+      .catch(this.setAxiosResponseError)
   }
   onTodoFormSubmit = evt =>{
     evt.preventDefault()
@@ -33,9 +34,7 @@ export default class App extends React.Component {
       .then(res =>{
         this.setState({ ...this.state, todos: res.data.data})
       })
-      .catch(err =>{
-        this.setState({...this.state, error: err.response.data.message })
-      })
+      .catch(this.setAxiosResponseError)
   }
   componentDidMount(){
     this.fetchAllTodos()
